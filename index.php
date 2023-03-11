16b497849818791c08743cae7afe6cb34cf80477
Amr Sharkas <asharkas@arabiclocalizer.com>
2023-03-11 04:44:02 +0200

    initial

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>iac PROJECT MANAGEMENT CONSULTANCY</title>
+</head>
+<body>
+	<div style="width: 100%; text-align: center;">
+<img src="/logo.jpeg" style="text-align: center; margin: 0 auto; width:50%;" />
+</div>
+<h2 style="width: 100%; text-align: center; font-size: 7rem; color:#333; "> UNDER CONSTRUCTION</h2>
+</body>
+</html>
